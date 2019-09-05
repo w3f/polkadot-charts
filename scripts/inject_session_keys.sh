86da4c426204edf0202bfab44e42e3ba75a74a95
Federico Gimenez <federico.gimenez@gmail.com>
2019-09-05 11:46:38 +0200

    extract public key from seed and usse it on injection call

--- scripts/inject_session_keys.sh
+++ scripts/inject_session_keys.sh
@@ -5,6 +5,19 @@ set -euo pipefail
 RPC_ENDPOINT=${1}
 STATEFULSET_NAME=${2}
 
+extract_public_key(){
+    local key_type=${1}
+    local key_seed=${2}
+
+    if [ "$key_type" = "gran" ]; then
+        CRYPTO=ed25519
+    else
+        CRYPTO=sr25519
+    fi
+
+    echo -n $(subkey --"${CRYPTO}" inspect "${key_seed}" | grep "Public key" | cut -d':' -f2 | tr -d '[:space:]')
+}
+
 for key in /keys/*; do
     [ -e "$key" ] || continue
 
@@ -26,8 +39,10 @@ for key in /keys/*; do
     esac
 
     if [ ! -z "${KEY_TYPE}" ]; then
+        PUBLIC_KEY=$(extract_public_key "${KEY_TYPE}" "${KEY_SEED}")
+
         curl -H "Content-Type: application/json" \
-             --data '{ "jsonrpc":"2.0", "method":"author_insertKey", "params":["'"${KEY_TYPE}"'", "'"${KEY_SEED}"'"],"id":1 }' \
+             --data '{ "jsonrpc":"2.0", "method":"author_insertKey", "params":["'"${KEY_TYPE}"'", "'"${KEY_SEED}"'", "'"${PUBLIC_KEY}"'"],"id":1 }' \
              "${RPC_ENDPOINT}"
     fi
 done

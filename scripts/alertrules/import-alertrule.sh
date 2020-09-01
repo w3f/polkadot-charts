94e1d950579d9e198ef8f9e9745ae888835c9eae
ironoa <alessio.onori@gmail.com>
2020-09-01 11:05:07 +0200

    test fix

--- scripts/alertrules/import-alertrule.sh
+++ scripts/alertrules/import-alertrule.sh
@@ -4,7 +4,7 @@ CURRENT_DIR=$(dirname "$0")
 source ${CURRENT_DIR}/vars.sh
 
 cat ${CURRENT_DIR}/template/prefix.yaml > ${RULE_FILE_NAME}
-yq p ${SUBSTRATE_RULES_FOLDER}/alerting-rules.yaml spec | sed 's/{{ \$labels\.instance.*\n*\s*\t*}}/{{`{{ $labels.instance }}`}}/gm' - | sed 's/{{ $value.*\n*\s*\t*}}/{{`{{ $value }}`}}/gm' - | yq w - "spec.groups[*].rules[*].labels.origin" "{{ .Values.deploymentName }}" | sed "s/'{{ .Values.deploymentName }}'/{{ .Values.deploymentName }}/gm" - >> ${RULE_FILE_NAME}
+yq p ${SUBSTRATE_RULES_FOLDER}/alerting-rules.yaml spec | sed 's/{{ \$labels\.instance.*\n*\s*\t*}}/{{`{{ $labels.instance }}`}}/g' - | sed 's/{{ $value.*\n*\s*\t*}}/{{`{{ $value }}`}}/g' - | yq w - "spec.groups[*].rules[*].labels.origin" "{{ .Values.deploymentName }}" | sed "s/'{{ .Values.deploymentName }}'/{{ .Values.deploymentName }}/g" - >> ${RULE_FILE_NAME}
 cat ${CURRENT_DIR}/template/postfix.yaml >> ${RULE_FILE_NAME}
 mv ${RULE_FILE_NAME} ${PROJECT_ROOT}/${CHART_FOLDER}
 

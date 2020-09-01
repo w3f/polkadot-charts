94539e78bd0005dffa19ac307c05914b5607187c
ironoa <alessio.onori@gmail.com>
2020-09-01 08:55:12 +0200

    fixed typo

--- scripts/alertrules/import-substrate.sh
+++ scripts/alertrules/import-substrate.sh
@@ -3,4 +3,4 @@
 CURRENT_DIR=$(dirname "$0")
 source ${CURRENT_DIR}/vars.sh
 
-git colone -b ${SUBSTRATE_VER} https://github.com/paritytech/substrate.git ${SUBSTRATE_FOLDER}
+git clone -b ${SUBSTRATE_VER} https://github.com/paritytech/substrate.git ${SUBSTRATE_FOLDER}

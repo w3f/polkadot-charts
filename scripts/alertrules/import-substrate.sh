fb972deb395b643ca7afdc9bd1be7c868644f30f
ironoa <alessio.onori@gmail.com>
2020-09-02 08:23:04 +0200

    migrated substrate-alertrules chart to dedicated repo

--- scripts/alertrules/import-substrate.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-CURRENT_DIR=$(dirname "$0")
-source ${CURRENT_DIR}/vars.sh
-
-git clone -b ${SUBSTRATE_VER} https://github.com/paritytech/substrate.git ${SUBSTRATE_FOLDER}
--- scripts/alertrules/import.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CURRENT_DIR=$(dirname "$0")
-
-${CURRENT_DIR}/import-substrate.sh
-${CURRENT_DIR}/import-alertrule.sh
-${CURRENT_DIR}/import-alertrule-test.sh
-
-rm -rf ${SUBSTRATE_FOLDER}/substrate
